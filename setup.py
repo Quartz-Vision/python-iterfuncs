15abfedcbfffaf8f954a38076a1cf22a4be92a5a
Lia Va <mistickusya.2012@gmail.com>
2024-03-25 08:36:02 -0600

    v0.1.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(ROOT / "requirements.txt") as fr:
 
 setuptools.setup(
     name="iterfuncs",
-    version="0.2.2",
+    version="0.1.0",
     author="liava",
     author_email="liava@tuta.io",
     description="Collection of iteration, caching and async tools",
