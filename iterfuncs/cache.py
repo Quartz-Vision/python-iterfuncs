52fcd346aebf3da03c91e426c38638683467aa79
Lia Va <mistickusya.2012@gmail.com>
2024-04-17 23:58:44 -0600

    add new cache methods

--- iterfuncs/cache.py
+++ iterfuncs/cache.py
@@ -1,11 +1,9 @@
 import weakref
-from collections.abc import Awaitable
-from functools import lru_cache as _lru_cache
 from functools import wraps
 from time import time
 from typing import Any, Callable
 
-from iterfuncs.typing import DecoratorFactoryT, IdT, MethodT
+from iterfuncs.typing import AbsDecoratorFT, IdT
 
 
 def make_hash(*args, **kwargs):
@@ -59,18 +57,27 @@ def ttl_cache[
 
             return tmp
 
+        def delete(*args: P.args, **kwargs: P.kwargs):
+            cache_key = key(*args, **kwargs)
+            if cache_key in cache:
+                del cache[cache_key]
+
+        setattr(wrapper, "delete", delete)
+
         return wrapper
 
     return ttl_decorator
 
 
+def copy_cache_methods(source, target):
+    cache_methods = ["delete"]
+    for method_name in cache_methods:
+        setattr(target, method_name, getattr(source, method_name, lambda *_, **__: None))
+
+
 def async_cache_adapter[
-    **P, **DP, DT
-](
-    cache_decorator_factory: DecoratorFactoryT[
-        P, Callable[DP, Callable[[], Awaitable[DT]]]
-    ]
-) -> DecoratorFactoryT[P, Callable[DP, Awaitable[DT]]]:
+    **P
+](cache_decorator_factory: AbsDecoratorFT[P]) -> AbsDecoratorFT[P]:
     """
     Should be used only for async functions. Every time it will return a !new! coroutine that returns cached value,
         so it should not be used inside other adapters that provide additional cache capabilities.
@@ -85,12 +92,12 @@ def async_cache_adapter[
     nothing = object()
 
     def transformed_decorator_factory(*d_args: P.args, **d_kwargs: P.kwargs):
-        def decorator(func: Callable[DP, Awaitable]):
+        def decorator(func):
             @cache_decorator_factory(*d_args, **d_kwargs)
             def update_coro(*args, **kwargs):
                 data: Any = nothing
 
-                async def get_coro() -> DT:
+                async def get_coro():
                     nonlocal data
                     if data is nothing:
                         data = await func(*args, **kwargs)
@@ -99,8 +106,10 @@ def async_cache_adapter[
                 return get_coro
 
             @wraps(func)
-            async def wrapped_func(*args: DP.args, **kwargs: DP.kwargs):
+            async def wrapped_func(*args, **kwargs):
                 return await update_coro(*args, **kwargs)()
+
+            copy_cache_methods(update_coro, wrapped_func)
 
             return wrapped_func
 
@@ -110,10 +119,8 @@ def async_cache_adapter[
 
 
 def method_cache_adapter[
-    **P, **DP, DT
-](cache_decorator_factory: DecoratorFactoryT[P, MethodT[DP, DT]]) -> DecoratorFactoryT[
-    P, MethodT[DP, DT]
-]:
+    **P
+](cache_decorator_factory: AbsDecoratorFT[P]) -> AbsDecoratorFT[P]:
     """
     Should be used only for instance methods and be the top level adapter (wrap all other adapters),
     so it can prevent them from handling `self`
@@ -133,22 +140,26 @@ def method_cache_adapter[
     """
 
     def transformed_decorator_factory(*d_args: P.args, **d_kwargs: P.kwargs):
-        def decorator(func: MethodT[DP, DT]):
+        def decorator(func):
             @wraps(func)
-            def wrapped_method(self, *args: DP.args, **kwargs: DP.kwargs) -> DT:
+            def wrapped_method(self, *args, **kwargs):
                 # If we had a strong reference to self the instance would never die
                 self_weak = weakref.ref(self)
 
-                @wraps(func)
                 @cache_decorator_factory(*d_args, **d_kwargs)
-                def cached_method(_, *args, **kwargs):
+                def cached_method(*args, **kwargs):
                     return func(self_weak(), *args, **kwargs)
+
+                wrapped_cached_method = wraps(func)(cached_method)
+                copy_cache_methods(cached_method, wrapped_cached_method)
 
                 # Replace the method with the cached one, so it
                 # will be called next time without cache initialization
-                setattr(self, func.__name__, cached_method)
+                setattr(self, func.__name__, wrapped_cached_method)
 
-                return cached_method(None, *args, **kwargs)
+                return wrapped_cached_method(*args, **kwargs)
+
+            copy_cache_methods(None, wrapped_method)
 
             return wrapped_method
 
@@ -157,9 +168,47 @@ def method_cache_adapter[
     return transformed_decorator_factory
 
 
-def amethod_cache_adapter(cache_decorator_factory):
+def amethod_cache_adapter[
+    **P
+](cache_decorator_factory: AbsDecoratorFT[P]) -> AbsDecoratorFT[P]:
     return method_cache_adapter(async_cache_adapter(cache_decorator_factory))
 
 
-def lru_cache(maxsize: int) -> IdT:
-    return _lru_cache(maxsize)
+def lru_cache[**P, T](size: int = -1, key: Callable[..., int] = make_hash) -> IdT:
+    """
+    Creates a function with size-limited cache.
+
+    :param key: function that generates cache key. By default, it uses make_hash
+    :param size: maximum number of items in cache. If -1, cache is unlimited
+    """
+
+    def ttl_decorator(func: Callable[P, T]) -> Callable[P, T]:
+        cache = {}
+
+        @wraps(func)
+        def wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
+            nonlocal cache
+
+            cache_key = key(*args, **kwargs)
+            if cache_key in cache:
+                return cache[cache_key]
+
+            if size > 0 and len(cache) >= size:
+                item = next(iter(cache))
+                del cache[item]
+
+            tmp = func(*args, **kwargs)
+            cache[cache_key] = tmp
+
+            return tmp
+
+        def delete(*args: P.args, **kwargs: P.kwargs):
+            cache_key = key(*args, **kwargs)
+            if cache_key in cache:
+                del cache[cache_key]
+
+        setattr(wrapper, "delete", delete)
+
+        return wrapper
+
+    return ttl_decorator
